c4f906ca5efb24d3645c411dc75b4f48da60ce50
Sagnik Dutta <sagdutt@gmail.com>
2016-04-11 02:05:36 +0530

    Fixed background image problem

--- templates/login.php
+++ templates/login.php
@@ -10,7 +10,10 @@
 		<!-- Latest compiled and minified JavaScript -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 
-		<link href='https://fonts.googleapis.com/css?family=Allura' rel='stylesheet' type='text/css'>	
+		<link href='https://fonts.googleapis.com/css?family=Allura' rel='stylesheet' type='text/css'>
+			
+		<script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+		<script src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 	</head>
 	<style type="text/css">
 		body {
